eb17a7271cf2f286c53776d81a931ecc62b11a2c
A <avilavate@live.in>
2020-01-02 14:43:47 +0530

    Azure SQL Sample

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzureWebApp_SQL_Service.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
